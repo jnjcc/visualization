b86aca9e49ab4ca10614e206aecf648eb33e6033
jnjcc <jnjcc@live.com>
2014-09-05 19:56:36 +1200

    Rescale attribute scores to make them comparable

--- basic/nba.r
+++ basic/nba.r
@@ -1,9 +1,17 @@
-require("reshape2");
+require("reshape2"); # for melt()
+require("plyr"); # for ddply()
+require("scales"); # for rescale()
 require("ggplot2");
 
 nba.raw <- read.csv("../data/nba.tsv", sep = '\t', header = TRUE);
 
-nba.frame <- melt(nba.raw, id.vars = "Name", variable.name = "Attribute",
+nba.melt <- melt(nba.raw, id.vars = "Name", variable.name = "Attribute",
                   value.name = "Score");
 
-nba.plot <- ggplot(nba.frame, aes(Attribute, Name, fill = Score)) + geom_tile();
+nba.frame <- ddply(nba.melt, .(Attribute), transform, Rescale = rescale(Score));
+
+nba.plot <- ggplot(nba.frame, aes(Attribute, Name, fill = Rescale)) +
+    geom_tile() +
+    scale_fill_gradient(low = "white", high = "steelblue", guide = FALSE) +
+    scale_x_discrete(expand = c(0, 0)) +
+    scale_y_discrete(expand = c(0, 0));
